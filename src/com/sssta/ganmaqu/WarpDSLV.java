39447fbbeec59a84dc3e8e6a7ab662a8a9fae21a
fydx <lbgg918@gmail.com>
2013-08-03 22:10:57 +0800

    Add PlaceName display

--- src/com/sssta/ganmaqu/WarpDSLV.java
+++ src/com/sssta/ganmaqu/WarpDSLV.java
@@ -1,36 +1,37 @@
 package com.sssta.ganmaqu;
 
 import java.io.InputStream;
-import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
-
-import junit.framework.Test;
 
 import org.apache.http.util.EncodingUtils;
 import org.json.JSONException;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.mobeta.android.dslv.DragSortListView;
-import com.mobeta.android.dslv.DragSortController;
+
 
 public class WarpDSLV extends ListActivity {
 
+  //  private ArrayAdapter<String> adapter;
     private ArrayAdapter<String> adapter;
     private String jsonString;
     private String[] array;
     private ArrayList<String> list;
     private List<place> places;
-
+    private Button button_toMap;
     private DragSortListView.DropListener onDrop =
         new DragSortListView.DropListener() {
             @Override
@@ -69,31 +70,29 @@ public class WarpDSLV extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.warp_main);
+      
+        ArrayList<HashMap<String, String>> mylist = new ArrayList<HashMap<String, String>>();
         
         DragSortListView lv = (DragSortListView) getListView(); 
 
         lv.setDropListener(onDrop);
         lv.setRemoveListener(onRemove);
         lv.setDragScrollProfile(ssProfile);
-
-        array = getResources().getStringArray(R.array.countries);
-        list = new ArrayList<String>(Arrays.asList(array));
-        /*   list = new ArrayList<String>();
-        list.add("上午 ： 汤姆熊欢乐世界");
-        list.add("中午 ： 骡马市必胜客");
-        list.add("下午 ： 骡马市民生百货");*/
-        lv.setOnItemClickListener(new OnItemClickListener() {
-
+        button_toMap = (Button)findViewById(R.id.button_toMap);
+        button_toMap.setOnClickListener(new OnClickListener() {
+			
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
+			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Toast.makeText(getApplicationContext(), "This is "+ String.valueOf(arg2+1) + " item", Toast.LENGTH_SHORT).show();
+				Intent intent = new Intent();
+				intent.setClass(getApplicationContext(), MapActivity.class);
+				startActivity(intent);
+				
 			}
-        	
 		});
-        adapter = new ArrayAdapter<String>(this, R.layout.list_item_handle_left, R.id.text, list);
-        setListAdapter(adapter);
+        /**
+   		 *read from assets json files	
+         */
         jsonString = getFromAssets("test.json");
         Log.i("file_content",jsonString);
         try {
@@ -108,9 +107,49 @@ public class WarpDSLV extends ListActivity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+        //array = getResources().getStringArray(R.array.countries);
+        //list = new ArrayList<String>(Arrays.asList(array));
+        list= new ArrayList<String>();
+        for (int i = 0; i < places.size(); i++) {
+        
+        	list.add(places.get(i).getShopName() + "\n" + places.get(i).getMainType() + "  "+ places.get(i).getDetailType());
+
+		}
+     
+    /*adapter = new SimpleAdapter(this, //activity
+				mylist,//ArrayList<HashMap>
+				R.layout.list_item_handle_left,// what is listitem ? 
+				//  key
+				new String[] { "name", "type" },
+				// TextView ID
+				new int[] { R.id.text_name, R.id.text_type });
+		               // display
+	//	   lv.setAdapter(adapter);
+        setListAdapter(adapter);
+        /*   list = new ArrayList<String>();
+        list.add("上午 ： 汤姆熊欢乐世界");
+        list.add("中午 ： 骡马市必胜客");
+        list.add("下午 ： 骡马市民生百货");*/
+        
+        lv.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplicationContext(), "This is "+ String.valueOf(adapter.getItem(arg2)) + " item", Toast.LENGTH_SHORT).show();
+			}
+        	
+		});
+  
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item_handle_left, R.id.text, list);
+        setListAdapter(adapter);
+        
+       
         
         
     }
+    //从assets中读取数据
     public String getFromAssets(String fileName){  
         String result = "";  
             try {  
