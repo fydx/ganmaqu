ac58e78e3a0b8193b5c4911b348c95af9cdeef1f
fydx <lbgg918@gmail.com>
2013-08-08 15:23:53 +0800

    Add route review

--- src/com/sssta/ganmaqu/RouteListActivity.java
+++ src/com/sssta/ganmaqu/RouteListActivity.java
@@ -1,11 +1,13 @@
 package com.sssta.ganmaqu;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import net.tsz.afinal.FinalDb;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -16,6 +18,7 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 public class RouteListActivity extends Activity {
+	FinalDb db, db_user;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,49 +26,55 @@ public class RouteListActivity extends Activity {
 		setContentView(R.layout.activity_route_list);
 		StringBuffer detailString = new StringBuffer();
 		ArrayList<HashMap<String, String>> mylist = new ArrayList<HashMap<String, String>>();
-		ListView listView_route = (ListView)findViewById(R.id.listView_route);
-		FinalDb db = FinalDb.create(this);
-		FinalDb db_user = FinalDb.create(this);
-		List<place> places= db.findAll(place.class);
+		ListView listView_route = (ListView) findViewById(R.id.listView_route);
+		 db = FinalDb.create(this);
+		 db_user = FinalDb.create(this);
+		List<place> places = db.findAll(place.class);
 		Log.i("DB item num", String.valueOf(places.size()));
-		User user = db_user.findById(1,	User.class);
-		int route_num = user.getRoute_num()+1;
+		User user = db_user.findById(1, User.class);
+		int route_num = user.getRoute_num() + 1;
 		/**
 		 * 由于现在数据库User是 (1,1)，so，k从1开始，因为已经加了1，所以k从2开始，终止值是getRoute_num+1
 		 */
-	
-		for(int k=2 ; k<=route_num ; k++)
-		{
-		places = db.findAllByWhere(place.class, "route_id = " + String.valueOf(k));
-		Log.i("item num in route", String.valueOf(places.size()));
-		HashMap<String, String> map = new HashMap<String, String>();
-		map.put("time", "2013-8-1");
-		
-		for (int i=0;i<places.size()-1;i++) {
-			detailString.append(places.get(i).getShopName()).append("-->" );	
+
+		for (int k = 2; k <= route_num; k++) {
+			places = db.findAllByWhere(place.class,
+					"route_id = " + String.valueOf(k));
+			Log.i("item num in route", String.valueOf(places.size()));
+			HashMap<String, String> map = new HashMap<String, String>();
+			map.put("time", "2013-8-1");
+
+			for (int i = 0; i < places.size() - 1; i++) {
+				detailString.append(places.get(i).getShopName()).append("-->");
+			}
+			detailString.append(places.get(places.size() - 1).getShopName());
+			map.put("detail", detailString.toString());
+			mylist.add(map);
 		}
-		detailString.append(places.get(places.size()-1).getShopName());
-		map.put("detail", detailString.toString());
-		mylist.add(map);
-		}
-		SimpleAdapter mSchedule = new SimpleAdapter(this, //activity
-				mylist,
-				R.layout.list_item_route_list,// ListItem
-				//  Key
+		SimpleAdapter mSchedule = new SimpleAdapter(this, // activity
+				mylist, R.layout.list_item_route_list,// ListItem
+				// Key
 				new String[] { "time", "detail" },
 				// ListItem TextView ID
 				new int[] { R.id.text_time, R.id.text_detail });
-		               // 
+		//
 
-		        listView_route.setAdapter(mSchedule);
-		 listView_route.setOnItemClickListener(new OnItemClickListener() {
+		listView_route.setAdapter(mSchedule);
+		listView_route.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 					long arg3) {
 				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(getApplicationContext(), WarpDSLV.class);
+				List<place> places_temp = db.findAllByWhere(place.class,
+						"route_id = " + String.valueOf(arg2+2));
 				
-				
+				intent.putExtra("places",(Serializable)places_temp);
+				startActivity(intent);
+			
+
 			}
 		});
 	}
