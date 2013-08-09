026cc2907502f36634795b60fea25c9368e5cf1b
fydx <lbgg918@gmail.com>
2013-08-09 16:30:19 +0800

    Change Button Style

--- src/com/sssta/ganmaqu/MainActivity.java
+++ src/com/sssta/ganmaqu/MainActivity.java
@@ -71,6 +71,7 @@ public class MainActivity extends Activity {
 			});
 	        numberWheel.setCurrentItem(3);
 	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
