df20f00bf78060d53aa15225db46ae8742450982
fydx <lbgg918@gmail.com>
2013-08-10 11:25:49 +0800

    Add marker in user's location

--- src/com/sssta/ganmaqu/MapActivity.java
+++ src/com/sssta/ganmaqu/MapActivity.java
@@ -1,13 +1,23 @@
 package com.sssta.ganmaqu;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -25,15 +35,37 @@ import android.widget.Toast;
 public class MapActivity extends Activity {
 	private dataFromJs data;
 	private String jsString;
+	private LocationManager locationManager;
+	private Location location;
+	private String provider;
+	private WebView mapView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_map);
+		/**
+		 * 设置LocationManager 设置地理位置服务
+		 */
+		// 获取LocationManager服务
+				locationManager = (LocationManager) this
+						.getSystemService(Context.LOCATION_SERVICE);
+				// 获取Location Provider
+				getProvider();
+				// 如果未设置位置源，打开GPS设置界面
+				openGPS();
+				// 获取位置
+				location = locationManager.getLastKnownLocation(provider);
+				// 显示位置信息到文字标签
+				
+				// 注册监听器locationListener，第2、3个参数可以控制接收gps消息的频度以节省电力。第2个参数为毫秒，
+				// 表示调用listener的周期，第3个参数为米,表示位置移动指定距离后就调用listener
+				locationManager.requestLocationUpdates(provider, 2000, 10,
+						locationListener);
 		final TextView routeTextView;
 		routeTextView = (TextView)findViewById(R.id.text_route);
 		data= new dataFromJs();
-		final WebView mapView = (WebView) findViewById(R.id.mapView);
+		mapView = (WebView) findViewById(R.id.mapView);
 		final List<place> places = (List<place>) getIntent().getSerializableExtra(
 				"places");
 		Log.i("places nums", String.valueOf(places.size()));
@@ -55,8 +87,8 @@ public class MapActivity extends Activity {
 	    mapView.addJavascriptInterface(data, "dataFromJs");
 		mapView.loadUrl("file:///android_asset/index.html");
 		// mapView.loadUrl("file:///android_asset/js.html");
-		Log.i("route", "javascript:calcRoute2("+String.valueOf(places.get(0).getPos_y())+","+
-		String.valueOf(places.get(0).getPos_x()+")"));
+//		Log.i("route", "javascript:calcRoute2("+String.valueOf(places.get(0).getPos_y())+","+
+//		String.valueOf(places.get(0).getPos_x()+")"));
 		Button jsButton = (Button) findViewById(R.id.test_button);
 //		mapView.loadUrl("javascript:calcRoute2("+String.valueOf(places.get(0).getPos_y())+","+
 //				String.valueOf(places.get(0).getPos_x()+")"));
@@ -98,8 +130,9 @@ public class MapActivity extends Activity {
 		
 			}
 		};
-		timer_2.schedule(task_2, 3000 * 1);
-		
+		timer_2.schedule(task_2, 1000 * 1);
+		//地图加载完毕后 update location
+		updateWithNewLocation(location);
 		//mapView.loadUrl("javascript:getRouteInfo()");
 		jsButton.setText("run");
 		jsButton.setOnClickListener(new OnClickListener() {
@@ -132,7 +165,103 @@ public class MapActivity extends Activity {
 		getMenuInflater().inflate(R.menu.map, menu);
 		return true;
 	}
-	
+	// 判断是否开启GPS，若未开启，打开GPS设置界面
+		private void openGPS() {
+			if (locationManager
+					.isProviderEnabled(android.location.LocationManager.GPS_PROVIDER)
+					|| locationManager
+							.isProviderEnabled(android.location.LocationManager.NETWORK_PROVIDER)) {
+				Toast.makeText(this, "位置源已设置！", Toast.LENGTH_SHORT).show();
+				return;
+			}
+			Toast.makeText(this, "位置源未设置！", Toast.LENGTH_SHORT).show();
+			// 转至GPS设置界面
+			Intent intent = new Intent(Settings.ACTION_SECURITY_SETTINGS);
+			startActivityForResult(intent, 0);
+		}
+
+		// 获取Location Provider
+		private void getProvider() {
+			// 构建位置查询条件
+			Criteria criteria = new Criteria();
+			// 查询精度：高
+			criteria.setAccuracy(Criteria.ACCURACY_FINE);
+			// 是否查询海拨：否
+			criteria.setAltitudeRequired(false);
+			// 是否查询方位角:否
+			criteria.setBearingRequired(false);
+			// 是否允许付费：是
+			criteria.setCostAllowed(true);
+			// 电量要求：低
+			criteria.setPowerRequirement(Criteria.POWER_LOW);
+			// 返回最合适的符合条件的provider，第2个参数为true说明,如果只有一个provider是有效的,则返回当前provider
+			provider = locationManager.getBestProvider(criteria, true);
+		}
+
+		// Gps消息监听器
+		private final LocationListener locationListener = new LocationListener() {
+			// 位置发生改变后调用
+			public void onLocationChanged(Location location) {
+
+				updateWithNewLocation(location);
+			}
+
+			// provider被用户关闭后调用
+			public void onProviderDisabled(String provider) {
+				updateWithNewLocation(null);
+			}
+
+			// provider被用户开启后调用
+			public void onProviderEnabled(String provider) {
+
+			}
+
+			// provider状态变化时调用
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+
+			}
+
+		};
+
+		// Gps监听器调用，处理位置信息
+		private void updateWithNewLocation(Location location) {
+			String latLongString;
+			double lat = 0;
+			double lng = 0;
+//			TextView myLocationText = (TextView) findViewById(R.id.text_location);
+			if (location != null) {
+				 lat = location.getLatitude();
+				lng = location.getLongitude();
+				latLongString = "纬度:" + lat + "\n经度:" + lng;
+			} else {
+				latLongString = "无法获取地理信息";
+			}
+			Toast.makeText(getApplicationContext(), "您当前的位置是: " + "\n" + latLongString + "\n"
+					, Toast.LENGTH_LONG).show();
+			mapView.loadUrl("javascript:addmarker("+String.valueOf(lat)+ ","+String.valueOf(lng) +" )");
+			
+//			myLocationText.setText("您当前的位置是:/n" + latLongString + "/n"
+//					+ getAddressbyGeoPoint(location));
+			
+		}
+
+	/*	// 获取地址信息
+		private List<Address> getAddressbyGeoPoint(Location location) {
+			List<Address> result = null;
+			// 先将Location转换为GeoPoint
+			// GeoPoint gp=getGeoByLocation(location);
+			try {
+				if (location != null) {
+					// 获取Geocoder，通过Geocoder就可以拿到地址信息
+					Geocoder gc = new Geocoder(this, Locale.getDefault());
+					result = gc.getFromLocation(location.getLatitude(),
+							location.getLongitude(), 1);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return result;
+		}*/
 }
 
 
