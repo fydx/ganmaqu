b27db546aebc685246f2d386a76f3b87ff83d9dd
fydx <lbgg918@gmail.com>
2013-09-04 17:27:15 +0800

    improve settings page

--- src/com/sssta/ganmaqu/MainActivity.java
+++ src/com/sssta/ganmaqu/MainActivity.java
@@ -108,7 +108,7 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Intent intent = new Intent();
-				intent.setClass(getApplicationContext(), DislikeActivity.class);
+				intent.setClass(getApplicationContext(),SettingsActivity.class);
 				startActivity(intent);
 			}
 		});
--- src/com/sssta/ganmaqu/SettingsActivity.java
+++ src/com/sssta/ganmaqu/SettingsActivity.java
@@ -1,15 +1,34 @@
 package com.sssta.ganmaqu;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
 
 public class SettingsActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_settings);
+		Button button_account = (Button)findViewById(R.id.button_account);
+		Button button_prefer = (Button)findViewById(R.id.button_prefer);
+		Button button_about = (Button)findViewById(R.id.button_about);
+		button_prefer.setOnClickListener( new  OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(SettingsActivity.this, DislikeActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 	@Override
