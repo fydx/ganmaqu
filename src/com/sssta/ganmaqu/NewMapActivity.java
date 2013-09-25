8822a4909c3b209b8941a028935e62199a614620
fydx <lbgg918@gmail.com>
2013-09-25 23:26:23 +0800

    Debug map

--- src/com/sssta/ganmaqu/NewMapActivity.java
+++ src/com/sssta/ganmaqu/NewMapActivity.java
@@ -1,20 +1,24 @@
 package com.sssta.ganmaqu;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.baidu.mapapi.BMapManager;
 import com.baidu.mapapi.map.ItemizedOverlay;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.OverlayItem;
 import com.baidu.mapapi.search.MKAddrInfo;
 import com.baidu.mapapi.search.MKBusLineResult;
 import com.baidu.mapapi.search.MKDrivingRouteResult;
+import com.baidu.mapapi.search.MKPoiInfo;
 import com.baidu.mapapi.search.MKPoiResult;
 import com.baidu.mapapi.search.MKSearch;
 import com.baidu.mapapi.search.MKSearchListener;
@@ -23,6 +27,7 @@ import com.baidu.mapapi.search.MKSuggestionResult;
 import com.baidu.mapapi.search.MKTransitRouteResult;
 import com.baidu.mapapi.search.MKWalkingRouteResult;
 import com.baidu.platform.comapi.basestruct.GeoPoint;
+import com.sssta.ganmaqu.SettingsFragment.loginTask;
 
 public class NewMapActivity extends Activity {
 	// 地图相关
@@ -31,30 +36,38 @@ public class NewMapActivity extends Activity {
 	Button mBtnReverseGeoCode = null; // 将坐标反编码为地址
 	Button mBtnGeoCode = null; // 将地址编码为坐标
 	MKSearch mSearch = null; // 搜索模块，也可去掉地图模块独立使用
+	 ArrayList<MKPoiInfo> mkpoi = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		DemoApplication app = (DemoApplication) this.getApplication();
 		//BMapManager 必须在setContentview前面初始化，否则报错
+		   if (app.mBMapManager == null) {
+	             app.mBMapManager = new BMapManager(this);
+	             app.mBMapManager.init(DemoApplication.strKey,new DemoApplication.MyGeneralListener());
+	         }
 		setContentView(R.layout.activity_new_map);
-	
+		
+          mkpoi = new ArrayList<MKPoiInfo>();
 		// 地图初始化
 		mMapView = (MapView) findViewById(R.id.bmapsView);
 		mMapView.getController().enableClick(true);
-		mMapView.getController().setZoom(12);
+		mMapView.getController().setZoom(14);
 
         /**
-         * 设定地图中心点
+         * 设定地图中心点 （西安钟楼）
          */
-        GeoPoint p = new GeoPoint((int)(34.265733 * 1E6), (int)(116.400191* 1E6));
+        GeoPoint p = new GeoPoint((int)(34.265733 * 1E6), (int)(108.953906* 1E6));
         mMapView.getController().setCenter(p);
         
 		// 初始化搜索模块，注册事件监听
 		mSearch = new MKSearch();
+		
 		mSearch.init(app.mBMapManager, new MKSearchListener() {
 			@Override
 			public void onGetPoiDetailSearchResult(int type, int error) {
+				
 			}
 
 			public void onGetAddrResult(MKAddrInfo res, int error) {
@@ -105,7 +118,42 @@ public class NewMapActivity extends Activity {
 			}
 
 			public void onGetPoiResult(MKPoiResult res, int type, int error) {
-
+				 // 错误号可参考MKEvent中的定义
+                if (error != 0 || res == null) {
+                    Toast.makeText(NewMapActivity.this, "抱歉，未找到结果", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                // 将地图移动到第一个POI中心点
+                if (res.getCurrentNumPois() > 0) {
+                    // 将poi结果显示到地图上
+                    MyPoiOverlay poiOverlay = new MyPoiOverlay(NewMapActivity.this, mMapView, mSearch);
+                    
+                  
+                    mkpoi.add(res.getPoi(0));
+                    
+                    poiOverlay.setData(mkpoi);
+                    
+               //     poiOverlay.setData(res.getAllPoi());
+                    mMapView.getOverlays().clear();
+                    mMapView.getOverlays().add(poiOverlay);
+                    mMapView.refresh();
+                    //当ePoiType为2（公交线路）或4（地铁线路）时， poi坐标为空
+                    for( MKPoiInfo info : res.getAllPoi() ){
+                    	if ( info.pt != null ){
+                    		mMapView.getController().animateTo(info.pt);
+                    		break;
+                    	}
+                    }
+                } else if (res.getCityListNum() > 0) {
+                	//当输入关键字在本市没有找到，但在其他城市找到时，返回包含该关键字信息的城市列表
+                    String strInfo = "在";
+                    for (int i = 0; i < res.getCityListNum(); i++) {
+                        strInfo += res.getCityListInfo(i).city;
+                        strInfo += ",";
+                    }
+                    strInfo += "找到结果";
+                    Toast.makeText(NewMapActivity.this, strInfo, Toast.LENGTH_LONG).show();
+                }
 			}
 
 			public void onGetDrivingRouteResult(MKDrivingRouteResult res,
@@ -135,7 +183,32 @@ public class NewMapActivity extends Activity {
 			}
 
 		});
-		mSearch.geocode("西安市","钟楼");
+		
+		 mSearch.poiSearchInCity("西安", 
+               "西安电子科技大学");
+		
+		 final Timer timer = new Timer();
+		 
+		 final TimerTask timerTask = new TimerTask() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				mSearch.poiSearchInCity("西安", 
+			               "西北工业大学");
+			}
+		};
+		new Thread()
+		{
+			@Override 
+			public void run()
+			{
+				Log.i("task","start");
+				timer.schedule(timerTask, 1000*10);
+				Log.i("task", "end");
+			}
+		}.start();
+	
 		
 		
 	}
