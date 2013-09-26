48fd9a0178f89ad3f2276cbc0e3337f27bc9c5bb
fydx <lbgg918@gmail.com>
2013-09-27 01:07:34 +0800

    Fixing map bugs

--- src/com/sssta/ganmaqu/NewMapActivity.java
+++ src/com/sssta/ganmaqu/NewMapActivity.java
@@ -2,35 +2,26 @@ package com.sssta.ganmaqu;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
-import android.R.drawable;
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.baidu.mapapi.BMapManager;
 import com.baidu.mapapi.map.ItemizedOverlay;
+import com.baidu.mapapi.map.MapController;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.OverlayItem;
-import com.baidu.mapapi.search.MKAddrInfo;
-import com.baidu.mapapi.search.MKBusLineResult;
-import com.baidu.mapapi.search.MKDrivingRouteResult;
+import com.baidu.mapapi.map.PopupOverlay;
 import com.baidu.mapapi.search.MKPoiInfo;
-import com.baidu.mapapi.search.MKPoiResult;
 import com.baidu.mapapi.search.MKSearch;
-import com.baidu.mapapi.search.MKSearchListener;
-import com.baidu.mapapi.search.MKShareUrlResult;
-import com.baidu.mapapi.search.MKSuggestionResult;
-import com.baidu.mapapi.search.MKTransitRouteResult;
-import com.baidu.mapapi.search.MKWalkingRouteResult;
 import com.baidu.platform.comapi.basestruct.GeoPoint;
-import com.sssta.ganmaqu.SettingsFragment.loginTask;
 
 public class NewMapActivity extends Activity {
 	// 地图相关
@@ -41,7 +32,21 @@ public class NewMapActivity extends Activity {
 	MKSearch mSearch = null; // 搜索模块，也可去掉地图模块独立使用
 	 ArrayList<MKPoiInfo> mkpoi = null;
 	private List<place> places;
-
+	/**
+	 *  用MapController完成地图控制 
+	 */
+	private MapController mMapController = null;
+	private MyOverlay mOverlay = null;
+	private PopupOverlay   pop  = null;
+	private ArrayList<OverlayItem>  mItems = null; 
+	private TextView  popupText = null;
+	private View viewCache = null;
+	private View popupInfo = null;
+	private View popupLeft = null;
+	private View popupRight = null;
+	private Button button = null;
+	private MapView.LayoutParams layoutParam = null;
+	private OverlayItem mCurItem = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -58,42 +63,69 @@ public class NewMapActivity extends Activity {
 				"places");
 		Log.i("places nums", String.valueOf(places.size()));
 		
-		
-          mkpoi = new ArrayList<MKPoiInfo>();
+        mkpoi = new ArrayList<MKPoiInfo>();
 		// 地图初始化
 		mMapView = (MapView) findViewById(R.id.bmapsView);
-		mMapView.getController().enableClick(true);
-		mMapView.getController().setZoom(14);
-
+		  /**
+         * 获取地图控制器
+         */
+        mMapController = mMapView.getController();
+        /**
+         *  设置地图是否响应点击事件  .
+         */
+        mMapController.enableClick(true);
+        /**
+         * 设置地图缩放级别
+         */
+        mMapController.setZoom(14);
+        /**
+         * 显示内置缩放控件
+         */
+        mMapView.setBuiltInZoomControls(true);
+		
         /**
          * 设定地图中心点 （西安钟楼）
          */
         GeoPoint p = new GeoPoint((int)(34.265733 * 1E6), (int)(108.953906* 1E6));
-        mMapView.getController().setCenter(p);
+        mMapController.setCenter(p);
+        
+        
         //封装地点坐标到list中
-        List<GeoPoint> geoPoints = new ArrayList<GeoPoint>();
+        List<GeoPoint> geoPoints = null;
+        geoPoints =		new ArrayList<GeoPoint>();
         for (int i = 0; i < places.size(); i++) {
         	 geoPoints.add(new GeoPoint((int)(places.get(i).getPos_y()* 1E6), (int)(places.get(i).getPos_x()* 1E6)));
+        	 Log.i("places info in GeoPoint", places.get(i).getShopName());
 		}
-        List<Drawable> markers = new ArrayList<Drawable>();
+        List<Drawable> markers = null;
+        	markers =	new ArrayList<Drawable>();
         for (int i = 0; i < markers_id.length; i++) {
 			markers.add(getResources().getDrawable(markers_id[i]));
 		}
-        List<OverlayItem> overlayItems = new ArrayList<OverlayItem>();
+        List<OverlayItem> overlayItems =  null ;
+        overlayItems = new ArrayList<OverlayItem>();
        for (int i = 0; i < markers_id.length; i++) {
     	 OverlayItem  temp_overlayItem = new OverlayItem(geoPoints.get(i), places.get(i).getShopName(), places.get(i).getShopName());
     	   temp_overlayItem.setMarker(markers.get(i));
 		 overlayItems.add(temp_overlayItem);
 		 
      	}
+       Log.i("overlay items ", String.valueOf(overlayItems.size()));
      
-     //创建IteminizedOverlay  
-       OverlayTest itemOverlay = new OverlayTest(markers.get(0), mMapView);  
+//     //创建IteminizedOverlay  
+//       OverlayTest itemOverlay = null ;
+//       itemOverlay = new OverlayTest(markers.get(0), mMapView);  
+       /**
+   	 * 创建自定义overlay
+   	 */
+        mOverlay = new MyOverlay(markers.get(0),mMapView);	
+        
        //将IteminizedOverlay添加到MapView中  
-         
-       mMapView.getOverlays().clear();  
-       mMapView.getOverlays().add(itemOverlay);  
-       itemOverlay.addItem(overlayItems);
+         mOverlay.addItem(overlayItems);
+       clearOverlay(mMapView);
+       
+       mMapView.getOverlays().add(mOverlay);  
+       
        mMapView.refresh();
         
 		
@@ -115,7 +147,54 @@ public class NewMapActivity extends Activity {
 	                super.onTap(pt,mapView);  
 	                return false;  
 	        }  
-	}          
+	}
+	 public class MyOverlay extends ItemizedOverlay{
+
+			public MyOverlay(Drawable defaultMarker, MapView mapView) {
+				super(defaultMarker, mapView);
+			}
+			
+
+			@Override
+			public boolean onTap(int index){
+				OverlayItem item = getItem(index);
+				mCurItem = item ;
+			
+				return true;
+			}
+			
+			@Override
+			public boolean onTap(GeoPoint pt , MapView mMapView){
+				if (pop != null){
+	                pop.hidePop();
+	                mMapView.removeView(button);
+				}
+				return false;
+			}
+	    	
+	    }
+	 /**
+	     * 清除所有Overlay
+	     * @param view
+	     */
+	    public void clearOverlay(View view){
+	    	mOverlay.removeAll();
+	    	if (pop != null){
+	            pop.hidePop();
+	    	}
+	    //	mMapView.removeView(button);
+	    	mMapView.refresh();
+	    }
+	 /**
+	     * 重新添加Overlay
+	     * @param view
+	     */
+	    public void resetOverlay(View view){
+	    	clearOverlay(null);
+	    	//重新add overlay
+	    	mOverlay.addItem(mItems);
+	    	mMapView.refresh();
+	    }
 	@Override
     protected void onPause() {
         mMapView.onPause();
@@ -129,7 +208,13 @@ public class NewMapActivity extends Activity {
     }
     @Override
     protected void onDestroy() {
+    	
         mMapView.destroy();
+//        DemoApplication app = (DemoApplication) this.getApplication();
+//        if(app.mBMapManager!=null){  
+//            app.mBMapManager.destroy();  
+//            app.mBMapManager=null;  
+//        }  
         super.onDestroy();
     }
     @Override
